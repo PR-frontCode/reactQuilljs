435779a6df347e396fae0b4e5e6593926be11a80
paulo-cds <paulo.pdomingues@hotmail.com>
2023-01-23 10:40:37 -0300

    react Quilljs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useQuill } from 'react-quilljs'
+import 'quill/dist/quill.snow.css'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const { quill, quillRef } = useQuill();
+  const [htmlCode, setHtmlCode] = useState("")
+  const [control, setControl] = useState(false)
+
+  useEffect(()=>{
+    if(quill){
+      quill.on('text-change',()=>{
+        setHtmlCode(quillRef.current.firstChild.innerHTML)
+      })
+    }
+  },[quill])
+
+  useEffect(()=>{
+    if(quill && !control){
+      quill.clipboard.dangerouslyPasteHTML('<h1>Hello world!</h1>')
+      setControl(true)
+    }
+  },[quill])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Convertendo texto em código html com ReactQuilljs</h1>
+      <div style={{display:'flex', alignItems:'center', justifyContent:'space-evenly', width:'100%'}}>
+        <div style={{ width: '500px', height: '300px' }}>
+          <div ref={quillRef}/>
+        </div>
+        <div style={{ width: '500px', height: '300px', flexWrap:'wrap' }}>
+          <h3>Visualização do código HTML5</h3>
+          <div style={{width: '100%', height: '100%', flexWrap:'wrap', overflowY:'scroll', border:'1px solid black'}}>
+            {htmlCode}
+          </div>
+        </div>
+      </div>
+      <div style={{marginTop:'5rem'}}>
+        <div dangerouslySetInnerHTML={{__html:htmlCode}} />
+      </div>
     </div>
   );
 }
